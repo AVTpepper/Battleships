298b7c3cdea514585c404377140907bd0d4d1fcd
AVTpepper <Alexander.tastad@hotmail.com>
2022-11-08 22:09:09 +0000

    Update: trying new functions to create my playing board

--- run.py
+++ run.py
@@ -5,31 +5,54 @@
 
 print("Welcome to the game of Battleships!")
 
-board = []
+# board = []
 
-
-def create_board(board):
-    print("""
+print("""
     We want you to create a custom Battleship board.\n
     Normally a Battleship board consists of 10 rows and 10 columns.\n
     This time, you get to decide the battlefield.\n
     After every input you type into the game, press enter to continue.
     """)
-    board_rows = int(input("Type the amount of rows you would like to play with: "))
-    board_rows_confirm = input(f"You have selected {board_rows}. Would you like to continue? y/n: ")
-    board_columns = int(input("Type the amount of columns you would like to play with: "))
-    
-    for row in range(board_rows):
-        board.append(["_"] * board_columns)
-    # print(board)
-    letter = 0
-    for letter in range(board_rows):
-        print(chr(letter + 65), end = " | ")
-        for column in range(board_columns):
-            print(board[letter][column], end = " ")
-        letter += 1
+board_rows = int(input("Type the amount of rows you would like to play with: "))
+# board_rows_confirm = input(f"You have selected {board_rows}. Would you like to continue? y/n: ")
+board_columns = int(input("Type the amount of columns you would like to play with: "))
+
+def create_board(board_rows, board_columns):
+    """
+    Creates the board based on user input
+    """
+    return [["_" for count in range(board_columns)] for rows in range(board_rows)]
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+    print("")
+
+def test():
+    myboard = create_board(board_rows, board_columns)
+    # print_board(myboard)
+    myboard[1][2] = str(6)
+    print_board(myboard)
+
+test()
+
+
+
+# for x in range(0, board_rows):
+    # board.append(["_ " * board_rows])
+
+# print(board)
+
+
+# def create_board(board):
+    # for row in board:
+        # print((" ").join(row))
+
+
+# create_board(board)
         
+    #return [["_" for count in range(board_columns)] for count in range(board_rows)]
 
-create_board(board)
+#print(create_board(board_rows, board_columns))
 
-print(board[A,1])
+#create_board(board_rows, board_columns)
