6504329ae878d5380ebd2845acb97252e724387a
AVTpepper <Alexander.tastad@hotmail.com>
2022-11-11 09:02:56 +0000

    Add: function to add a player guess onto the board

--- run.py
+++ run.py
@@ -6,42 +6,82 @@
 print("Welcome to the game of Battleships!")
 
 
-print("""
-    We want you to create a custom Battleship board.\n
-    Normally a Battleship board consists of 10 rows and 10 columns.\n
-    This time, you get to decide the size of the battlefield.\n
-    After every input you type into the game, press enter to continue.
-    """)
+print("We want you to create a custom Battleship board.15")
+print("Normally a Battleship board consists of 10 rows and 10 columns.")
+print("This time, you get to decide the size of the battlefield.")
+print("After every input you type into the game, press enter to continue.")
+#add print message specifying that the board needs to be 10 or less.
 
-
-row = int(input("enter number of rows: "))
+#enter board size instead of rows and columns. 3 = 3x3, 4 = 4x4
+row = int(input("enter number of rows: ")) #check what is entered, with a message, higher that max, enter something less than or 10
 
 column = int(input("enter number of columns: "))
+
 rows = []
 columns = []
+game_board = " "
+
+
 
 def create_board():
     i = 1
     while i < (row + 1):
-        piece = str(i) + " " + ("-   " * column)
-        rows.append(piece)
+        piece = str(i)
+        dash = "-"
+        list_creation = [piece] + ([dash] * column)
+        rows.append(list_creation)
         i+=1
         game_board = "\n".join(str(item) for item in rows)
+    create_column_index()
     print(game_board)
+    # print(rows)
+
 
 
 def create_column_index():
     i = 1
     while i < (column + 1):
-        col_number = "  " + str(i)
+        col_number = "   " + str(i)
         columns.append(col_number)
         i += 1
         indexes = " ".join(str(index) for index in columns)
     print(indexes)
 
+def update_board(rows):
+    #guess_row = [int(input("enter row: "))]
+    #guess_column=[int(input("enter column: "))]
+    #guess = guess_row + guess_column
+    #print(guess)
+    #guesses = []
+    rows[(int(input("enter row: ")))-1][int(input("enter column: "))] = "O"
+    
 
-create_column_index()    
+    
+    # create_column_index()
+    # create_board()
+    # create_board()
+# rows[1][2] = "X"
+# print(game_board)
+
+
+# def place_ships():
+
+
+# def shots():
+
+
+# def run_game():
+
+# create_column_index()    
 create_board()
+
+rows[1][2]= "X"
+# print(rows)
+update_board(rows)
+# create_column_index()
+# create_board()
+
+
 
 """
 Another try at creating a functional board
