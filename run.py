fa48db1fc80a6151b8fdbd230c0db39545f3d242
AVTpepper <Alexander.tastad@hotmail.com>
2022-11-15 21:53:48 +0000

    Fix: removed all commented out code

--- run.py
+++ run.py
@@ -10,11 +10,10 @@ print("We want you to create a custom Battleship board.15")
 print("Normally a Battleship board consists of 10 rows and 10 columns.")
 print("This time, you get to decide the size of the battlefield.")
 print("After every input you type into the game, press enter to continue.")
-#add print message specifying that the board needs to be 10 or less.
 
-#enter board size instead of rows and columns. 3 = 3x3, 4 = 4x4
+
 board_size = int(input("enter board size: ")) 
-#check what is entered, with a message, higher that max, enter something less than or 10
+
 
 board = []
 
@@ -25,54 +24,22 @@ print(board)
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
 def print_board(board):
-    column = 1
-    #for column in board:
-        # index = "  " + alphabet[range(len(board))]
-    #column += 1
-    #print(index)
+    """
+    creates the board
+    """
     letters = alphabet[0: (board_size)]
     print(letters)
-    #for i in letters:
     print("  %s%s" % (" "," ".join(letters)))
-
-
-    #range(len(board))
     row_number = 1
     for row in board:
         if row_number <= 9:
             print(" %d|%s|" % (row_number, "|".join(row)))
         else:
             print("%d|%s|" % (row_number, "|".join(row)))
-       # print(" +-+-+-+-+-+")
         row_number += 1    
-        #for letter in alphabet:
-
-# ship_placement = []
-
-#def placing_ships(ship_placement):
- #   ship_placement = []
-  #  ships_placed = 0
-   # if board_size < 6:
-    #    total_ships = 1
-     #   print(f"There is {total_ships} ship in range commander, go shoot them down!")
-      #  while ships_placed != total_ships:
-       #     ship_row = randint(0, (board_size))
-        #    ship_col = randint(0, (board_size))
-         #   ship_location = [ship_row, ship_col]
-          #  ship_placement.append(ship_location)
-           # ships_placed += 1
-    #elif board_size <= 10:
-     #   total_ships = 3
-      #  print(f"There are {total_ships} ships in range commander, go shoot them down!")
-       # while ships_placed != total_ships:
-        #    ship_row = randint(0, (board_size))
-         #   ship_col = randint(0, (board_size))
-          #  ship_location = [ship_row, ship_col]
-           # ship_placement.append(ship_location)
-            #ships_placed += 1
-    #print(ship_placement)
-    # return ship_placement
+        
 
 
 def making_guesses():
@@ -125,141 +92,15 @@ def making_guesses():
             print_board(board)
             print("Congratulations Commander, you have sunk all the ships!")
             print("Victory is ours, set sails, and let's go home!")
-            break
-        
-            
+            break  
         turn += 1
         print_board(board)
-#        if ships_sunk == total_ships:
-#            print_board(board)
-#            print("Congratulations, you have sunk all the ships!")
-#            break
 
 
 print("Let's play Battleship!")
 print_board(board)
-#placing_ships(ship_placement)
 making_guesses()
 
 
 
 
-"""
-def random_row(board):
-    return randint(0, len(board))
-def random_col(board):
-    if board_size == 10:
-        return randint(0, len(board))
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-print(ship_row, ship_col)
-"""
-
-
-
-"""
-column = int(input("enter number of columns: "))
-
-rows = []
-columns = []
-game_board = " "
-
-
-
-def create_board():
-    i = 1
-    while i < (row + 1):
-        piece = str(i)
-        dash = "-"
-        list_creation = [piece] + ([dash] * column)
-        rows.append(list_creation)
-        i+=1
-        game_board = "\n".join(str(item) for item in rows)
-    create_column_index()
-    print(game_board)
-    # print(rows)
-
-
-
-def create_column_index():
-    i = 1
-    while i < (column + 1):
-        col_number = "   " + str(i)
-        columns.append(col_number)
-        i += 1
-        indexes = " ".join(str(index) for index in columns)
-    print(indexes)
-
-def update_board(rows):
-    #guess_row = [int(input("enter row: "))]
-    #guess_column=[int(input("enter column: "))]
-    #guess = guess_row + guess_column
-    #print(guess)
-    #guesses = []
-    rows[(int(input("enter row: ")))-1][int(input("enter column: "))] = "O"
-    
-
-    
-    # create_column_index()
-    # create_board()
-    # create_board()
-# rows[1][2] = "X"
-# print(game_board)
-
-
-# def place_ships():
-
-
-# def shots():
-
-
-# def run_game():
-
-# create_column_index()    
-create_board()
-
-rows[1][2]= "X"
-# print(rows)
-update_board(rows)
-# create_column_index()
-# create_board()
-
-
-Another try at creating a functional board
-"""
-
-"""
-row = int(input("Enter rows here: "))
-column = int(input("enter columns here: "))
-
-#Hidden_Board = [["-"]*
-
-
-Hidden_Pattern= [[' ']*8 for x in range(8)]
-Guess_Pattern= [[' ']*8 for x in range(8)]
-
-#print(Hidden_Pattern)
-
-Hidden_Board = [[" "]*row for x in range(column+1)]
-Board = [[" "]*row for x in range(column+1)]
-
-
-letters_to_number = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10}
-
-def print_board(Board):
-  letters=["A","B","C","D","E","F","G","H","I","J","K"]
-  column_letters=[]
-  i = 0
-  while i < column:
-    column_letters.append(letters[i])
-    i +=1
-  print(column_letters)
-  #print(Board)
-  row_num = 1
-  for row in Board:
-    print(row_num, "|-|".join(row))
-    row_num += 1
-    
-print_board(Board)
-"""
