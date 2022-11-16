6a9e32f8dce5db9a345c9d5dfc56860b3d01dd0f
AVTpepper <Alexander.tastad@hotmail.com>
2022-11-16 18:35:50 +0000

    Add: restart function

--- old_run.py
+++ old_run.py
@@ -8,30 +8,28 @@ from random import randint
 
 
     
-def introduction():
-    print("Welcome to the game of Battleships!")
-    print("We want you to create a custom Battleship board.15")
-    print("Normally a Battleship board consists of 10 rows and 10 columns.")
-    print("This time, you get to decide the size of the battlefield.")
-    print("After every input you type into the game, press enter to continue.")
 
-        
+print("Welcome to the game of Battleships!")
+print("We want you to create a custom Battleship board.")
+print("Normally a Battleship board consists of 10 rows and 10 columns.")
+print("This time, you get to decide the size of the battlefield.")
+print("After every input you type into the game, press enter to continue.")
+board_size = int(input("enter board size: "))
 
-        
 
 def get_board_size():
         
-    global board_size
+    global y
     global board
 
-    if board_size > 1 and board_size <= 10:
+    if y > 1 and y <= 10:
         print("perfect")
-    if board_size < 1 or board_size > 10:
+    if y < 1 or y > 10:
         raise Exception("Sorry, no numbers below 1 or higher than 10")
 
-    for x in range(board_size):
-        board.append(["-"] * board_size)
-    return board_size
+    for x in range(y):
+        board.append(["-"] * y)
+    return y
 
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -42,7 +40,7 @@ def print_board():
     creates the board
     """
     global board
-    letters = alphabet[0: (board_size)]
+    letters = alphabet[0: (y)]
     print("  %s%s" % (" "," ".join(letters)))
     row_number = 1
     for row in board:
@@ -54,40 +52,40 @@ def print_board():
 
 
 def making_ships():
-    global board_size
+    global y
     global board
     global ship_placement
     ships_placed = 0
     global ships_sunk
     global total_ships
     shots = 20
-    if board_size <= 2:
+    if y <= 3:
         shots = 2
         total_ships = 1
         print(f"There is {total_ships} ship in range commander, go shoot it down!")
         while ships_placed != total_ships:
-            ship_row = randint(1, (board_size))
-            ship_col = randint(1, (board_size))
+            ship_row = randint(1, (y))
+            ship_col = randint(1, (y))
             ship_location = [ship_row, ship_col]
             ship_placement.append(ship_location)
             ships_placed += 1
-    if board_size < 6:
+    elif y < 6 and y > 3 :
         total_ships = 3
         shots = 10
-        print(f"There is {total_ships} ship in range commander, go shoot it down!")
+        print(f"There are {total_ships} ships in range commander, go shoot them down!")
         while ships_placed != total_ships:
-            ship_row = randint(1, (board_size))
-            ship_col = randint(1, (board_size))
+            ship_row = randint(1, (y))
+            ship_col = randint(1, (y))
             ship_location = [ship_row, ship_col]
             ship_placement.append(ship_location)
             ships_placed += 1
-    elif board_size <= 10:
+    else:
         shots = 20
         total_ships = 10
         print(f"There are {total_ships} ships in range commander, go shoot them down!")
         while ships_placed != total_ships:
-            ship_row = randint(1, (board_size))
-            ship_col = randint(1, (board_size))
+            ship_row = randint(1, (y))
+            ship_col = randint(1, (y))
             ship_location = [ship_row, ship_col]
             ship_placement.append(ship_location)
             ships_placed += 1
@@ -98,13 +96,15 @@ def making_guesses():
     global ship_placement
     global ships_sunk
     global board_size
+    global total_ships
     
-    for turn in range((board_size*board_size) // 3):  
+    for turn in range((y*y) // 3):  
             # I want this to be in a second function, 
             # but Im stuck at how I can do that because of the variables 
             # I am using I have tried passing them through return___ and having them in paranthesis behind the def function(__)
-        shots = (board_size*board_size) // 3
+        shots = (y*y) // 3
         print(f"Commander, we have {shots - turn} shots left.")
+        print(f"Our scouting report says there are this many ships left: {total_ships - ships_sunk} ")
                 
                 
         guess_row = None
@@ -151,10 +151,10 @@ def making_guesses():
         elif (turn + 1) - shots == 0:
             print("Game Over, lets go back to the docks and restock ammunition")
             #break
-        elif (guess_row < 1 or guess_row > board_size) or (guess_col < 1 or guess_col > board_size):
+        elif (guess_row < 1 or guess_row > y) or (guess_col < 1 or guess_col > y):
             print("Commander, your coordinates are out of range!")
             print("Stop wasting our shots!")
-            print(f"Try shooting within rows 1-{board_size} and columns A-{alphabet[board_size - 1]}")
+            print(f"Try shooting within rows 1-{y} and columns A-{alphabet[y - 1]}")
         elif (board[guess_row - 1][guess_col - 1]) == "X" or (board[guess_row - 1][guess_col - 1]) == "O":
             print("Commander...You guessed that one already...")
         else:
@@ -164,30 +164,57 @@ def making_guesses():
             print_board()
             print("Congratulations Commander, you have sunk all the ships!")
             print("Victory is ours, set sails, and let's go home!")
-            #break 
+            break 
         turn += 1
                 
         print_board()
 
-introduction()
-
-board_size = int(input("enter board size: "))
+    
 
 
+def run_game():
+    global y
+    get_board_size()
+    print("Let's play Battleship!")
+    print_board()
+    making_ships()
+    making_guesses()
+
+def restart_game():
+    print("hello!")
+    play = input("Type yes to play again and no to quit: ").lower()
+    while True:
+        if play == "no":
+            exit()
+        elif play == "yes":
+            import sys
+            print("------------------------------------")
+            print("argv was",sys.argv)
+            print("sys.executable was", sys.executable)
+            print("restart now")
+
+            import os
+            os.execv(sys.executable, ['python'] + sys.argv)
+            print("------------------------------------")
+        else: 
+            print("please type either yes or no")
+            restart_game()
 
 
-
+y = board_size
 board = []
 ship_placement = []
 total_ships = 10
 print(total_ships)
 ships_sunk = 0
 
-get_board_size()
-print("Let's play Battleship!")
-print_board()
-making_ships()
-making_guesses()
+run_game()
+restart_game()
+
+y = board_size
+
+
+
 
 
 
